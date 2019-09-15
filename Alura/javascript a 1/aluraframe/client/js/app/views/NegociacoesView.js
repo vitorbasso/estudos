7e5295d1179654a01a07f8f24ecfccde94f41f3d
Vitor Basso <vitorbasso93@gmail.com>
2019-09-15 18:26:07 -0300

    js avancado 1 completo

--- Alura/javascript a 1/aluraframe/client/js/app/views/NegociacoesView.js
+++ Alura/javascript a 1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,10 +1,10 @@
-class NegociacoesView{
+class NegociacoesView extends View{
 
     constructor(elemento){
-        this._elemento = elemento;
+        super(elemento);
     }
 
-    _template(model){
+    template(model){
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -36,9 +36,5 @@ class NegociacoesView{
             </table>
         `;
 
-    }
-
-    update(model){
-        this._elemento.innerHTML = this._template(model);
     }
 } 
