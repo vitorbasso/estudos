05d48f22ada9bc502011129a5a05171745b70191
Vitor Basso <vitorbasso93@gmail.com>
2019-09-15 18:31:45 -0300

    final js avancado 1

--- Alura/javascript a 1/aluraframe/client/js/app/views/MensagemView.js
+++ Alura/javascript a 1/aluraframe/client/js/app/views/MensagemView.js
@@ -1,9 +1,5 @@
 class MensagemView extends View{
 
-    constructor(elemento){
-        super(elemento);
-    }
-
     template(model){
         return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : `<p></p>`;
     }
